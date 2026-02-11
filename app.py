d339bc574e3f98435f53edc5dde0d067d053dbb4
milesmacfarlane <40318591+milesmacfarlane@users.noreply.github.com>
2026-02-11 09:12:25 -0600

    Change sidebar logo width to 50
    Reduced the width of the sidebar logo from 150 to 50.

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def main():
     # Sidebar navigation
 with st.sidebar:
     st.image("https://media.7oaks.org/media/Default/fgg/36/West20Kildonan20Logo.jpg", 
-             width=150)
+             width=50)
     
     st.markdown("---")
         
